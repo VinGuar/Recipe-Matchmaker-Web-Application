4c46e5f5bae7329e393dda09402b8f2aea01b5db
VinGuar <vincentguarnieri1@gmail.com>
2023-08-08 10:56:28 -0400

    final

--- recipe/app/layout.js
+++ recipe/app/layout.js
@@ -8,6 +8,7 @@ export const metadata = {
   description: "Welcome to the Recipe Matchmaker! This wonderful tool helps you find the perfect recipes tailored to your individual needs from almost 200 thousand options!",
 }
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
